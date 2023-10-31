5244e2b5549d8f21fb32d52bb3661eae864416d3
MatthiasKebede <msk9862@nyu.edu>
2023-10-31 19:17:18 +0400

    10/31

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 console.log("check");
 
+var playButton = document.getElementById("PlayButton");
+var volumeButton = document.getElementById("VolButton");
+var currentaudio = document.getElementById("audio");
+
+playButton.addEventListener("click", playAudio);
+volumeButton.addEventListener("click", changeVolume);
+
+
+function playAudio() {
+    if (!currentaudio.paused) {
+        currentaudio.pause();
+    }
+    else {currentaudio.play()}
+}
+
+function changeVolume() {
+    currentaudio.volume += 0.1;
+    currentaudio.volume = (currentaudio.volume*10 % 10)/10;
+}
