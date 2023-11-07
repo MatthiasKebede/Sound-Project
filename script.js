d020b3b90503dc84c7e934227b01c1bfd5f7d2e2
MatthiasKebede <msk9862@nyu.edu>
2023-11-07 21:44:13 +0400

    End screen added

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ console.log("check");
 
 // // HTML Elements
 // Divs
+var contents = document.getElementById("content-area");
 var recorder = document.getElementById("Recorder");
 var tapecover = document.getElementById("Tapecover");
 var droptarget = document.getElementById("Droptarget");
+var afterword = document.getElementById("end-screen")
 // Buttons
 var recordButton = document.getElementById("RecButton");
 var volumeUp = document.getElementById("VolUp");
@@ -20,6 +22,8 @@ var tapeColor = document.getElementById("PlayColor");
 var tape1 = document.getElementById("Tape1");
 var tape2 = document.getElementById("Tape2");
 var tape3 = document.getElementById("Tape3");
+// Audio
+var click = document.getElementById("click"); click.volume = 0.25;
 // For animation
 var topWheel = document.getElementById("TopSpokes");
 var botWheel = document.getElementById("BotSpokes");
@@ -72,7 +76,9 @@ tape3.addEventListener("click", function() {
 // // Functions
 // Play/Pause audio, show button being pressed in
 function playAudio() {
-    // play a clicking sound?
+    // play a clicking sound
+    click.currentTime = 0;
+    click.play();
     if (!currentaudio.paused) {
         currentaudio.pause();
         pauseMode();
@@ -129,6 +135,9 @@ function resetAll() {
     tapecover.style.display = "block";
     droptarget.style.display = "none";
     recorder.style.marginLeft = "20%";
+    document.getElementById("Tapes").appendChild(document.getElementById("Chose-Tape-1"));
+    document.getElementById("Tapes").appendChild(document.getElementById("Chose-Tape-2"));
+    document.getElementById("Tapes").appendChild(document.getElementById("Chose-Tape-3"));
 }
 
 // Timestamp for audio
@@ -147,30 +156,52 @@ function timeStamp() {
     playtime.textContent = timestamp + " / " + endtime;
 }
 
-
-
-
-
-
-
-// Drag and drop a tape
+// Drag and drop a tape to submit your answer
 function dragStart(event) {
-    event.dataTransfer.setData("text", event.target.id);
-  }
-  
-//   function dragEnd(event) {
-//     droptarget.style.opacity = 0;
-//   }
-  
-  function allowDrop(event) {
+    event.dataTransfer.setData("text/plain", event.currentTarget.id);
+}
+function allowDrop(event) {
+    if (!event.currentTarget.children.length)
+        event.preventDefault();
+    else console.log("Submission box full");
+}
+function drop(event) {
     event.preventDefault();
-  }
-  
-  function drop(event) {
-    event.preventDefault();
-    const data = event.dataTransfer.getData("text");
+    const data = event.dataTransfer.getData("text/plain");
     const source = document.getElementById(data);
-    console.log(data);
-    event.target.appendChild(document.getElementById("Chose Tape 1"));
-    // function for final page or whatever
-  }
+    event.currentTarget.appendChild(source);
+    // Run function for final page (on a delay)
+    currentaudio.pause(); pauseMode();
+    setTimeout(function() {finalAnswer(source.id)}, 4000);
+    contents.classList.add("curtain");
+}
+
+// Submit answer and show results
+function finalAnswer(answer) {
+    console.log("You chose: Tape " + answer.slice(-1));
+    var percentage;
+    switch (answer) {
+        case "Chose-Tape-1":
+            // code block
+            percentage = 23;
+
+            break;
+        case "Chose-Tape-2":
+            // code block
+            percentage = 59;
+
+            break;
+        case "Chose-Tape-3":
+            // code block
+            percentage = 18;
+
+            break;
+        default:
+            console.log("answer error");
+    }
+    contents.style.display = "none";
+    currentaudio.pause(); pauseMode();
+    afterword.style.display = "block";
+    document.getElementById("result1").textContent = "You and " + percentage + "%" 
+    document.getElementById("result2").textContent = " of people chose: Tape " + answer.slice(-1);
+}
